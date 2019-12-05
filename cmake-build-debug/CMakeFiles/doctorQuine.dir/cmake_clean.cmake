c8672cf1cd907feda61a0cc87ccc5774fc64cdd1
matthieu CONTI <mconti@student.42.fr>
2019-12-05 11:50:33 +0100

    first

---
+++ cmake-build-debug/CMakeFiles/doctorQuine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/doctorQuine.dir/main.c.o"
+  "doctorQuine"
+  "doctorQuine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/doctorQuine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
