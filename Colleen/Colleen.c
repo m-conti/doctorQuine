7067b612ee957a71750cd26a75c33a9ec55bfdf2
matthieu CONTI <mconti@student.42.fr>
2019-12-05 13:15:36 +0100

    collen DONE

--- Colleen/Colleen.c
+++ Colleen/Colleen.c
@@ -1,8 +1,14 @@
 # include <stdio.h>
 
+// ma fonction print
+void	print(char *s) {
+	printf(s,10,10,47,47,10,9,10,9,10,10,10,9,9,10,10,9,47,47,10,9,9,34,s,34,10,9,10,9,10);
+}
+
 int		main(void) {
 
-	char	*s = "# include <stdio.h>%c%cint%c%cmain(void) {%c%c%cchar%c*s = %c%s%c;%c%cprintf(s,10,10,9,9,10,10,9,9,34,s,34,10,9,10,9,10);%c%creturn (0);%c}";
-	printf(s,10,10,9,9,10,10,9,9,34,s,34,10,9,10,9,10);
+	// ma string
+	char	*s = "# include <stdio.h>%c%c%c%c ma fonction print%cvoid%cprint(char *s) {%c%cprintf(s,10,10,47,47,10,9,10,9,10,10,10,9,9,10,10,9,47,47,10,9,9,34,s,34,10,9,10,9,10);%c}%c%cint%c%cmain(void) {%c%c%c%c%c ma string%c%cchar%c*s = %c%s%c;%c%cprint(s);%c%creturn (0);%c}";
+	print(s);
 	return (0);
 }
